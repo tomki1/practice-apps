1e2f1f9a94325b4479b380270797c51920b8e709
Kimberly Tom <kimberlyjtom@gmail.com>
2023-08-02 11:50:40 -0700

    database connection

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,4 +1,22 @@
 const mongoose = require("mongoose");
+mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.DB_NAME}`);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("we are connected");
+});
+
+const glossarySchema = new mongoose.Schema({
+  name: { type: String, unique: true },
+  definition: String
+});
+
+
+
+module.exports.db = db;
+
+
 
 // 1. Use mongoose to establish a connection to MongoDB
 // 2. Set up any schema and models needed by the app
--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -1,19 +1,42 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const db = require("./db.js");
+
 
 const app = express();
 
+app.use(express.json());
 // Serves up all static and generated assets in in a specified folder.
-app.use(express.static(path.join(__dirname, /* FILL ME IN */)));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
-/****
- *
- *
- * Other routes here....
- *
- *
- */
+// routes
+
+app.get('/words', (req, res) => {
+  res.send('get request to /words')
+
+})
+
+app.post('/words', (req, res) => {
+  res.send('post request to /words')
+
+})
+
+app.put('/words', (req, res) => {
+  res.send('put request to /words')
+})
+
+
+app.delete('/words', (req, res) => {
+  res.send('delete request to /words')
+})
+
+
+
+app.all('/*', (req, res) => {
+  res.send('hello world')
+})
+
 
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
